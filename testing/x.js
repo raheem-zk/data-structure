b20efd69ae2f31eeb1a130ee2d8f95dd98ee42c7
raheem-zk <muhammedraheem144@gmail.com>
2023-03-21 19:08:25 +0530

    duble LinkList deleting added

--- testing/x.js
+++ testing/x.js
@@ -1,3 +1,4 @@
+/*
 class Node {
     constructor (data){
         this.data = data;
@@ -80,3 +81,78 @@ obj.deleting(20);  // set ullathine kalayunu
 
 obj.insertAfter(30,777); //inserting new value
 obj.display();
+*/
+
+// DobleLinkedList
+
+class Node {
+    constructor(data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class DobleLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    addNode(data){
+        let newNode = new Node(data);
+        if(this.head == null ){
+            this.head = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+        }
+        this.tail = newNode;
+    // console.log(this.head);
+    }
+    display (){
+        let temp = this.head;
+        while(temp != null){
+            console.log(temp.data);
+            temp = temp.next;
+        }
+    }
+    displayReverse(){
+        let temp = this.tail;
+        while(temp != null ){
+            console.log(temp.data);
+            temp = temp.prev;
+        }
+    }
+    deleting(data){
+        let temp = this.head;
+        if (temp != null && temp.data == data){
+            this.head = temp.next;
+            this.head.prev = null;
+            // console.log(temp);
+            return;
+        }
+        while(temp != null && temp.data != data){
+            temp = temp.next;
+        }
+        if (temp == null) {
+            return; // If data not found, exit the method
+          }
+        if(temp == this.tail){
+            this.tail = temp.prev;
+            this.tail.next = null;
+            return;
+        }
+
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+    }
+}
+let obj = new DobleLinkedList();
+obj.addNode(10);
+obj.addNode(20);
+obj.addNode(30);
+obj.addNode(40);
+obj.addNode(50);
+
+// obj.deleting(50);
+obj.display();
+obj.displayReverse();
